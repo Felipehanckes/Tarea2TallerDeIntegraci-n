270b19760dc802bde74e245eb9d6935ed8183840
felipehanckes <fihanckes@uc.cl>
2020-05-02 19:21:28 -0400

    updated static_root

--- BurguerApiFelipeHanckes/settings.py
+++ BurguerApiFelipeHanckes/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = OS.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
